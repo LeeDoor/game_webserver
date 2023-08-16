2df590e0507168623932cc9b8b86607c11355a25
leonid <samoshenkoleonid@yandex.ru>
2023-08-16 19:56:50 +0300

    basic structure

---
+++ src/initializer/initializer.hpp
@@ -0,0 +1,11 @@
+#pragma once
+namespace initializer {
+
+class Initializer {
+public:
+    int init(int argc, char** argv);
+private:
+    int start_server();
+};
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "initializer/initializer.hpp"
+
+int main(int argc, char** argv) {
+    initializer::Initializer initializer;
+    initer.init(argc, argv);
+}
