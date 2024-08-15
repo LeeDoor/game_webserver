8271a33a3bd07af359e4ac32178c7a2534b2d6cf
leonid <samoshenkoleonid@yandex.ru>
2024-08-15 20:33:48 +0300

    tests_closed_room

--- tests/test_api_game_walk.cpp
+++ tests/test_api_game_walk.cpp
@@ -15,7 +15,7 @@ bool ValidCell(const gm::State& state, unsigned x, unsigned y){
     return it == terrain.end();
 }
 
-TEST_CASE("ApiMove", "[api][game][move]"){
+TEST_CASE("ApiMove", "[api][game][move][walk]"){
 	net::io_context ioc;
     tcp::socket socket{ioc};
     ConnectSocket(ioc, socket);
@@ -209,98 +209,71 @@ TEST_CASE("ApiMove", "[api][game][move]"){
                 .res = http::status::bad_request
             });
     }
-}
+    SECTION("in_prepared_room_walless"){
+        SessionData sd = CreateNewMatch(socket);
+        gm::State state = sd.state;
+        state.map_size = {2,2};
+        state.now_turn = sd.l1.login;
+        state.players[0].posX = 0;
+        state.players[0].posY = 0;
+        state.players[1].posX = 1;
+        state.players[1].posY = 0;
+        SetStateSuccess(socket, state, sd.sid);
+        // XXXXXXXXXX
+        //Y * * * * *
+        //Y * A * B *
+        //Y * * * * *
+        //Y *   *   *
+        //Y * * * * *
 
-TEST_CASE("ApiResign", "[api][game][resign]"){
-	net::io_context ioc;
-    tcp::socket socket{ioc};
-    ConnectSocket(ioc, socket);
-    
-    std::string ACCESS_DENIED = serializer::SerializeError("access_denied", "you have no access to do this action");
-    std::string SESSION_FINISHED = serializer::SerializeError("session_finished", "session is finished");
-
-    SECTION("success"){
-        SessionData sd = CreateNewMatch(socket);
-        um::Login& now_turn = sd.state.now_turn;
-        LoginData& ld = sd.l1.login == now_turn ? sd.l1 : sd.l2;
-        ResignSuccess(socket, ld.token, sd.sid);
-        INFO("resign is successful");
-        sm::PublicSessionData psd = PublicSessionDataSuccess(socket, sd.sid, sd.l1.token);
-        CHECK(psd == PublicSessionDataSuccess(socket, sd.sid, sd.l2.token));
-        CHECK(*psd.winner != *psd.loser);
-        CHECK(*psd.loser == ld.login);
-    }
-    SECTION("success_when_not_our_move"){
-        SessionData sd = CreateNewMatch(socket);
-        um::Login& now_turn = sd.state.now_turn;
-        LoginData& ld1 = sd.l1.login == now_turn ? sd.l1 : sd.l2;
-        LoginData& ld2 = sd.l1.login == now_turn ? sd.l2 : sd.l1;
-        ResignSuccess(socket, ld2.token, sd.sid);
-        INFO("resign is successful");
-        sm::PublicSessionData psd = PublicSessionDataSuccess(socket, sd.sid, sd.l1.token);
-        CHECK(psd == PublicSessionDataSuccess(socket, sd.sid, sd.l2.token));
-        CHECK(*psd.winner != *psd.loser);
-        CHECK(*psd.winner == ld1.login);
-        CHECK(*psd.loser == ld2.login);
-    }
-    SECTION("access_denied"){
-        SessionData sd = CreateNewMatch(socket);
-        LoginData ld = EnqueueNewPlayer(socket);
-        StringResponse response = Resign(socket, ld.token, sd.sid);
-        CheckStringResponse(response, {
-            .body = ACCESS_DENIED,
-            .res = http::status::bad_request
+        StringResponse response;
+        INFO("A cant walk into the enemy");
+        response = Walk(socket, {1, 0}, sd.l1.token, sd.sid);
+        CheckStringResponse(response,{
+            .body=WRONG_MOVE,
+            .res=http::status::bad_request
         });
-        gm::State state = SessionStateSuccess(socket, sd.l1.token, sd.sid);
-        CHECK(state == SessionStateSuccess(socket, sd.l2.token, sd.sid));
-    }
-    SECTION("cant_resign_twice"){
-        SessionData sd = CreateNewMatch(socket);
-        um::Login& now_turn = sd.state.now_turn;
-        LoginData& ld = sd.l1.login == now_turn ? sd.l1 : sd.l2;
-        ResignSuccess(socket, ld.token, sd.sid);
-        INFO("resign is successful");
-        sm::PublicSessionData psd = PublicSessionDataSuccess(socket, sd.sid, sd.l1.token);
-        CHECK(psd == PublicSessionDataSuccess(socket, sd.sid, sd.l2.token));
-        CHECK(*psd.winner != *psd.loser);
-        CHECK(*psd.loser == ld.login);
+
+        INFO("A can walk at cell below");
+        WalkSuccess(socket, {0, 1}, sd.l1.token, sd.sid);
         
-        StringResponse response = Resign(socket, sd.l1.token, sd.sid);
-        CheckStringResponse(response, {
-            .body=SESSION_FINISHED,
+        // * * * * *
+        // *   * B *
+        // * * * * *
+        // * A *   *
+        // * * * * *
+
+        INFO("B cant walk right");
+        response = Walk(socket, {0, 2}, sd.l2.token, sd.sid);
+        CheckStringResponse(response,{
+            .body=WRONG_MOVE,
             .res=http::status::bad_request
         });
 
-        response = Resign(socket, sd.l2.token, sd.sid);
-        CheckStringResponse(response, {
-            .body=SESSION_FINISHED,
-            .res=http::status::bad_request
-        });
-    }
-    SECTION("cant_move_after_resign"){
-        SessionData sd = CreateNewMatch(socket);
-        um::Login& now_turn = sd.state.now_turn;
-        LoginData& ld = sd.l1.login == now_turn ? sd.l1 : sd.l2;
-        ResignSuccess(socket, ld.token, sd.sid);
-        INFO("resign is successful");
-        sm::PublicSessionData psd = PublicSessionDataSuccess(socket, sd.sid, sd.l1.token);
-        CHECK(psd == PublicSessionDataSuccess(socket, sd.sid, sd.l2.token));
-        CHECK(*psd.winner != *psd.loser);
-        CHECK(*psd.loser == ld.login);
-        
-        gm::Player& player1 = sd.state.players[0].login == sd.l1.login ? sd.state.players[0] : sd.state.players[1];
-        gm::Player& player2 = sd.state.players[1].login == sd.l1.login ? sd.state.players[0] : sd.state.players[1]; 
+        INFO("B can walk left, where opponent just was");
+        WalkSuccess(socket, {0, 0}, sd.l2.token, sd.sid);
 
-        StringResponse response = Walk(socket, {player1.posX + 1, player1.posY}, sd.l1.token, sd.sid);
-        CheckStringResponse(response, {
-            .body=SESSION_FINISHED,
+        // * * * * *
+        // * B *   *
+        // * * * * *
+        // * A *   *
+        // * * * * *
+
+        INFO("A cant go down");
+        response = Walk(socket, {2, 0}, sd.l1.token, sd.sid);
+        CheckStringResponse(response,{
+            .body=WRONG_MOVE,
             .res=http::status::bad_request
         });
 
-        response = Walk(socket, {player2.posX + 1, player2.posY}, sd.l1.token, sd.sid);
-        CheckStringResponse(response, {
-            .body=SESSION_FINISHED,
+        INFO("A cant go above on B");
+        response = Walk(socket, {0, 0}, sd.l1.token, sd.sid);
+        CheckStringResponse(response,{
+            .body=WRONG_MOVE,
             .res=http::status::bad_request
         });
+
+        INFO("A can walk right");
+        WalkSuccess(socket, {1, 1}, sd.l1.token, sd.sid);
     }
 }
